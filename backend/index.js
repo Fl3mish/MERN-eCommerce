c2a3d934fb1755124735b8dbe672a975810594e5
Fl3mish <mitch.pauwels@proton.me>
2024-08-12 00:44:01 +0100

    Backend connecting to Mongo | Frontend - setup tailwind

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,21 @@
-console.log("BACKEND");
+// packages
+import path from "path";
+import e from "express";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+
+// utils
+import connectDB from "./config/db.js";
+dotenv.config();
+const app = e();
+const port = process.env.PORT || 5000;
+
+connectDB();
+
+app.get("/", (req, res) => res.send("Succesfully connected to the Homepage"));
+
+app.use(e.json());
+app.use(e.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+app.listen(port, () => console.log(`Listening on port ${port}`));
